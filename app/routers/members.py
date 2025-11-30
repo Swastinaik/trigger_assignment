86bfe427813c0a95033be4bd25516d73c8d784ab
swasti <swastinaik273@gmail.com>
2025-11-30 16:20:46 +0530

    apply the unique constraint

--- app/routers/members.py
+++ app/routers/members.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Form, Depends
+from fastapi import APIRouter, Form, Depends, HTTPException
 from typing import Annotated
 from sqlmodel import select
 from app.models import Member
@@ -11,8 +11,13 @@ router = APIRouter(prefix="/members", tags=["members"])
 
 @router.post('/')
 async def create_member(phone: Annotated[str, Form()], name: Annotated[str, Form()], session: AsyncSession = Depends(get_session)):
-    if(phone is None or name is None):
+    if phone is None or name is None or len(phone) != 10:
         raise HTTPException(status_code=400, detail='Bad Request')
+    member = select(Member).where(Member.phone == phone)
+    result = await session.execute(member)
+    existing_member = result.scalar_one_or_none()
+    if existing_member:
+        raise HTTPException(status_code=400, detail='Member with this phone number already exists')
     member = Member(name=name, phone=phone, join_date=datetime.now())
     session.add(member)
     await session.commit()
@@ -25,7 +30,3 @@ async def get_members(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Member))
     members = result.scalars().all()
     return members
-
-
-
-    
